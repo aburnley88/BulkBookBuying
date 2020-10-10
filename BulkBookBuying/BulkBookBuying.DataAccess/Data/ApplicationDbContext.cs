2b63af68392a03c7246877502728727418065b6f
aBurnley <aburnleylvx@outlook.com>
2020-10-10 12:50:46 -0400

    AddProductModelToDb

--- BulkBookBuying/BulkBookBuying.DataAccess/Data/ApplicationDbContext.cs
+++ BulkBookBuying/BulkBookBuying.DataAccess/Data/ApplicationDbContext.cs
@@ -11,5 +11,6 @@ namespace BulkBookBuying.DataAccess.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
