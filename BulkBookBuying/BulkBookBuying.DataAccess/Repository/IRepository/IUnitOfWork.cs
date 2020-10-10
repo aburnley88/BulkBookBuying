ff80976011f8e86c7b09aa519e8abcc1509f020d
aBurnley <aburnleylvx@outlook.com>
2020-10-10 12:20:16 -0400

    AddCoverTypeHomeController/IndexView +dataupdates

--- BulkBookBuying/BulkBookBuying.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkBookBuying/BulkBookBuying.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -7,7 +7,7 @@ namespace BulkBookBuying.DataAccess.Repository.IRepository
     public interface IUnitOfWork : IDisposable
     {
         ICategoryRepository Category { get; }
-       // ICoverTypeRepository CoverType { get; }
+        ICoverTypeRepository CoverType { get; }
        // IProductRepository Product { get; }
         //ICompanyRepository Company { get; }
         //IApplicationUserRepository ApplicationUser { get; }
