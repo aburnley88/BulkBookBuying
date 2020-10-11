15bebfa6a5f216e71fec2b55492461733fa38a64
aBurnley <aburnleylvx@outlook.com>
2020-10-11 14:46:42 -0400

    added ApplicationUser class and migrated

---
+++ BulkBookBuying/BulkBookBuying.Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace BulkBookBuying.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+
+        [NotMapped] 
+        public string Role { get; set; }
+  
+    }
+}
