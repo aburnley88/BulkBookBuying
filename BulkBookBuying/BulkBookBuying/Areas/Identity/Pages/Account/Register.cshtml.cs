4ca6b8edd425e0a99e5063f94fb755311f1a008b
aBurnley <aburnleylvx@outlook.com>
2020-10-25 06:14:10 -0400

    finished Identity scaffolding

--- BulkBookBuying/BulkBookBuying/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BulkBookBuying/BulkBookBuying/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using BulkBookBuying.DataAccess.Repository.IRepository;
 using BulkBookBuying.Models;
 using BulkBookBuying.Utility;
+using BulkyookBuying.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -80,13 +82,34 @@ namespace BulkBookBuying.Areas.Identity.Pages.Account
             public string State { get; set; }
             public string PostalCode { get; set; }
             public string PhoneNumber { get; set; }
-           
+            public int? CompanyId { get; set; }
+            public string Role { get; set; }
+
+            public IEnumerable<SelectListItem> CompanyList { get; set; }
+              public IEnumerable<SelectListItem> RoleList { get; set; }
+
 
         }
 
         public async Task OnGetAsync(string returnUrl = null)
         {
+
+            //dropdown for admin to see comanyList and RoleList
             ReturnUrl = returnUrl;
+            Input = new InputModel()
+            {
+                CompanyList = _unitOfWork.Company.GetAll().Select(x=> new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                }),
+                 RoleList = _roleManager.Roles.Where(u=> u.Name!=SD.Role_User_Indi).Select(x=> x.Name).Select(i=> new SelectListItem
+                {
+                    Text = i,
+                    Value = i
+                })
+            };
+
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
 
@@ -96,7 +119,20 @@ namespace BulkBookBuying.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser
+                {
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    CompanyId = Input.CompanyId,
+                    StreetAddress = Input.StreetAddress,
+                    City = Input.City,
+                    State = Input.State,
+                    PostalCode = Input.PostalCode,
+                    Name = Input.Name,
+                    PhoneNumber = Input.PhoneNumber,
+                    Role = Input.Role
+
+                };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
@@ -118,7 +154,19 @@ namespace BulkBookBuying.Areas.Identity.Pages.Account
                     {
                         await _roleManager.CreateAsync(new IdentityRole(SD.Role_User_Indi)); 
                     }
-                    await _userManager.AddToRoleAsync(user, SD.Role_Admin); 
+                   if(user.Role == null)
+                   {
+                        await _userManager.AddToRoleAsync(user, SD.Role_User_Indi) ;
+
+                   }
+                    else
+                    {
+                        if(user.CompanyId > 0)
+                        {
+                            await _userManager.AddToRoleAsync(user, SD.Role_User_Comp);
+                        }
+                        await _userManager.AddToRoleAsync(user, user.Role);
+                    }
                     //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     //var callbackUrl = Url.Page(
@@ -136,8 +184,18 @@ namespace BulkBookBuying.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
+                        if (user.Role == null)
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                            return LocalRedirect(returnUrl);
+
+                        }
+                        else
+                        {
+                            return RedirectToAction("Index", "User", new {Area ="Admin"});
+                        }
+                            //admin is registering a new user
+                        
                     }
                 }
                 foreach (var error in result.Errors)
